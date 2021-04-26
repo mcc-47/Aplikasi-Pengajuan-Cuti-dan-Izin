5d1b568da2d9a3cc6eee2984316eb2a4f44b9669
Jaka Brajadenta <jakabrajadenta@gmail.com>
2021-04-26 11:33:01 +0700

    initial client project

---
+++ src/main/java/com/mii/client/ClientAplikasiPengajuanCutiDanIzinApplication.java
@@ -0,0 +1,13 @@
+package com.mii.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientAplikasiPengajuanCutiDanIzinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientAplikasiPengajuanCutiDanIzinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mii/client/ClientAplikasiPengajuanCutiDanIzinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mii.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientAplikasiPengajuanCutiDanIzinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
